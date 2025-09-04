e8a6a3999519a02f043f6d4d6e22d3e8599a0d49
yangweijie <917647288@qq.com>
2025-09-04 10:58:07 +0800

    删除历史代码

--- tests/TestCase.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Tests;
-
-use LaravelZero\Framework\Testing\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase {}
--- ~/mcp-shrimp-task-manager/data/WebGUI.md
+++
@@ -1,1 +0,0 @@
-[Task Manager UI](http://localhost:58471?lang=zh-TW)
